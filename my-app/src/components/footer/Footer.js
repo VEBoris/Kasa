5135e1b4df08b1ef6a16dc4ca8d757d7275415b9
VEBoris <Visuya@outlook.fr>
2023-05-19 21:02:11 +0200

    add footer

---
+++ my-app/src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css'
+import logoFooter from '../../assets/logo/logo_footer.png'
+
+function Footer(){
+    return(
+        <footer className="footer">
+            <img src={logoFooter} className="logo_footer" alt="logo"></img>
+            <h2 className="copyrights">© 2020 Kasa. All rights reserved</h2>
+        </footer>
+    );
+}
+
+export default Footer
